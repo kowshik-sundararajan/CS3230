ff663e14d4661e6b68f72133c7f14404afb037a8
Kowshik Sundararajan <ksundararajan@authentic8.com>
2017-10-29 15:24:37 +0800

    Implementation of Unbounded Knapsack

---
+++ src/UnboundedKnapsack.java
@@ -0,0 +1,74 @@
+/*
+ * The Unbounded Knapsack program implements the traditional Knapsack algorithm using Dynamic Programming
+ * with unlimited number of each item.
+ *
+ * @author  Kowshik Sundararajan
+ * @version 1.0
+ * @since   2017 - 10 - 19
+ */
+
+import java.util.*;
+
+class UnboundedKnapsack {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter the number of items");
+        int N = sc.nextInt();
+        //System.out.println("Enter the capacity of the knapsack");
+        int C = sc.nextInt();
+        int[] W = new int[N];
+        int[] V = new int[N];
+        //System.out.println("Enter the weight and value of each item");
+        for (int i = 0; i < N; i++) {
+            W[i] = sc.nextInt();
+            V[i] = sc.nextInt();
+        }
+
+        int solution = solve(N, W, V, C);
+        //System.out.println("The maximum value that the knapsack can store is: ");
+        System.out.println(solution);
+    }
+
+    /**
+     * Function to find the maximum value that can fit in the knapsack
+     *
+     * @param N The number of items
+     * @param W Array containing the weights of the items
+     * @param V Array containing the values of the items
+     * @param C Capacity of the knapsack
+     * @return The maximum value that the knapsack can hold
+     */
+    private static int solve(int N, int[] W, int[] V, int C) {
+        int m[][] = new int[N + 1][C + 1];
+
+        for (int row = 0; row <= N; row++) {
+            m[row][0] = 0;
+        }
+
+        for (int col = 0; col <= C; col++) {
+            m[0][col] = 0;
+        }
+
+        for (int row = 1; row <= N; row++) {
+            for (int col = 1; col <= C; col++) {
+                if (W[row - 1] <= col)
+                    m[row][col] = Math.max(m[row][col - W[row - 1]] + V[row - 1], m[row - 1][col]);
+                else
+                    m[row][col] = m[row - 1][col];
+            }
+        }
+
+        /*
+         * Print out the bottom-up table
+         * for (int i = 0; i < N; i++) {
+         *    for (int j = 0; j < C + 1; j++) {
+         *        System.out.print(m[i][j] + "   ");
+         *    }
+         *    System.out.println();
+         * }
+         */
+
+        return m[N][C];
+    }
+}
