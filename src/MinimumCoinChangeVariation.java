88a0209baf5282f6825ab186719db63a5a3e8fd8
Kowshik Sundararajan <ksundararajan@authentic8.com>
2017-11-03 14:07:42 +0800

    Implementation of Minimum Coin Change with a variation.

---
+++ src/MinimumCoinChangeVariation.java
@@ -0,0 +1,65 @@
+/*
+ * The Birthday Problem program implements a variation of the minimum coin change
+ * problem using Dynamic Programming.
+ * The variation is that the user can enter one denomination greater than the largest
+ * denomination.
+ *
+ * @author  Kowshik Sundararajan
+ * @version 1.0
+ * @since   2017 - 11 - 3
+ */
+
+import java.util.Scanner;
+
+class MinimumCoinChangeVariation {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //System.out.println("Enter the number of totals");
+        int N = sc.nextInt();
+        //System.out.println("Enter a denomination greater than 20");
+        int K = sc.nextInt();
+        //System.out.println("Enter the different totals")
+        int[] P = new int[N];
+        for (int i = 0; i < N; i++) {
+            P[i] = sc.nextInt();
+        }
+
+        int solution = solve(N, K, P);
+        // System.out.print("The minimum number of coins required to make up the total is:\t");
+        System.out.println(solution);
+    }
+
+    /**
+     * Function to find the maximum value that can fit in the knapsack
+     *
+     * @param N The number of totals
+     * @param K The user entered denomination
+     * @param P Array containing the different totals
+     * @return The minimum number of coins required
+     */
+    private static int solve(int N, int K, int[] P) {
+        int denominations[] = {1, 4, 6, 9, 20, K};
+        int numOfCoins = 0;
+
+        for (int coin : P) {
+            int[] m = new int[coin + 1];
+
+            for (int i = 1; i <= coin; i++) {
+                m[i] = Integer.MAX_VALUE;
+            }
+
+            for (int i = 1; i <= coin; i++) {
+                for (int denomination : denominations) {
+                    if (denomination <= i) {
+                        int temp = m[i - denomination];
+                        if (temp != Integer.MAX_VALUE && (temp + 1 < m[i]))
+                            m[i] = temp + 1;
+                    }
+                }
+            }
+            numOfCoins += m[coin];
+        }
+        return numOfCoins;
+    }
+}
